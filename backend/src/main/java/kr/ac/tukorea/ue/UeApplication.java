76624da4e32df47b4ed3b3bc7ec4dfdee0c506b3
K-S-H-2000 <agooy00@naver.com>
2026-09-04 12:37:30 +0900

    스프링 프로젝트 추가

---
+++ backend/src/main/java/kr/ac/tukorea/ue/UeApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.tukorea.ue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/kr/ac/tukorea/ue/UeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.tukorea.ue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
